3d9a8919e9e49f2b04e96d32c4eeb3b789c63c5c
dcfarrell <59483721+dcfarrell@users.noreply.github.com>
2025-03-03 22:18:27 -0600

    Created Hamburger Menu
    Made a hamburger menu. Next I will add  sections.

---
+++ script.js
@@ -0,0 +1,8 @@
+// Purpose: Target two elements and toggle a class on both of them when a button is clicked.
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
